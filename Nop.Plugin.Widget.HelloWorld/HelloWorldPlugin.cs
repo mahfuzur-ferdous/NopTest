ba259471f3a870f1e0077a7e86971f02f5b04c93
mahfuzur-ferdous <mahfuzur.rahmanferdous@brainstation-23.com>
2025-04-10 16:09:50 +0600

    Created Manufacturer Plugin and Widget

---
+++ Nop.Plugin.Widget.HelloWorld/HelloWorldPlugin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+
+namespace Nop.Plugin.Widget.HelloWorld;
+public class HelloWorldPlugin: BasePlugin
+{
+    public override async Task InstallAsync()
+    {
+        //Logic during installation goes here...
+
+        await base.InstallAsync();
+    }
+
+    public override async Task UninstallAsync()
+    {
+        //Logic during uninstallation goes here...
+
+         await base.UninstallAsync();
+    }
+}
---
+++ Plugins/Nop.Plugin.Widget.HelloWorld/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Nop.Plugin.Widget.HelloWorld;
+
+public class Class1
+{
+
+}
